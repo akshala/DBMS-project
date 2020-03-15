926d5e02ee48eb4926a224668e31f7a736195a73
Rachit Mittal <rachit18302@iiitd.ac.in>
2020-03-15 13:20:14 +0530

    modified scrapping

--- transfer_market_scraping.py
+++ transfer_market_scraping.py
@@ -9,17 +9,31 @@ from bs4 import BeautifulSoup
 option = webdriver.ChromeOptions()
 option.add_argument(" — incognito")
 
-browser = webdriver.Chrome(executable_path = '/home/akshala/Documents/IIITD/fourthSem/Misc/web scraping/chromedriver_linux64/chromedriver', chrome_options=option)
-url = "https://www.transfermarkt.co.in/cristiano-ronaldo/profil/spieler/8198"
+browser = webdriver.Chrome(executable_path = '/home/rachit/Downloads/chromedriver_linux64/chromedriver', chrome_options=option)
+url = "https://www.transfermarkt.co.in/premier-league/startseite/wettbewerb/GB1"
 page = browser.get(url)
 
-season_stats_elements = browser.find_elements_by_xpath("//a[@class='trackingLDWidget']")
+teamLinks_elements = browser.find_elements_by_xpath("//a[@class='vereinprofil_tooltip tooltipstered']")
+# print(season_stats_elements)
+teamLinks = [x.get_attribute("href") for x in teamLinks_elements]
 
-season_stats = [x.text for x in season_stats_elements[6:12]]
+teamLinks = list(dict.fromkeys(teamLinks))
+teamLinks = teamLinks[:20]
+for i in teamLinks:
+    url = i
+    page = browser.get(url)
+    playerLinks_elements = browser.find_elements_by_xpath("//a[@class='spielprofil_tooltip tooltipstered']")
+    playerLinks= [x.get_attribute("href") for x in playerLinks_elements]
+    for j in playerLinks:
+        url = j
+        page = browser.get(url)
+        season_stats_elements = browser.find_elements_by_xpath("//a[@class='trackingLDWidget']")
 
-print("Games ", season_stats[0])
-print("Yellow cards ", season_stats[1])
-print("Goals ", season_stats[2])
-print("Second yellows ", season_stats[3])
-print("Assists ", season_stats[4])
-print("Red cards ", season_stats[5])
+        season_stats = [x.text for x in season_stats_elements[6:12]]
+
+        print("Games ", season_stats[0])
+        print("Yellow cards ", season_stats[1])
+        print("Goals ", season_stats[2])
+        print("Second yellows ", season_stats[3])
+        print("Assists ", season_stats[4])
+        print("Red cards ", season_stats[5])
