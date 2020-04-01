94edbcb9faa377c8c77f32681378d1680a6cf6fa
Rachit Mittal <rachit18302@iiitd.ac.in>
2020-04-01 20:17:13 +0530

    Season Player scrapping

--- transfer_market_scraping.py
+++ transfer_market_scraping.py
@@ -3,13 +3,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.common.exceptions import TimeoutException
+import csv
 import pandas as pd
 from bs4 import BeautifulSoup
 
 option = webdriver.ChromeOptions()
 option.add_argument(" — incognito")
 
-browser = webdriver.Chrome(executable_path = '/home/akshala/Documents/IIITD/fourthSem/Misc/web scraping/chromedriver_linux64/chromedriver', chrome_options=option)
+browser = webdriver.Chrome(executable_path = '/home/rachit/Downloads/chromedriver_linux64/chromedriver', chrome_options=option)
 url = "https://www.transfermarkt.co.in/premier-league/startseite/wettbewerb/GB1"
 page = browser.get(url)
 
@@ -20,7 +21,9 @@ teamLinks = [x.get_attribute("href") for x in teamLinks_elements]
 teamLinks = list(dict.fromkeys(teamLinks))
 teamLinks = teamLinks[:20]
 print(teamLinks)
-
+row = []
+row.append(["S.No","Name", "Games_Played","Goals","Assists","Yellow_cards","Red_cards","Club"])
+cntt = 0
 while (not teamLinks):
     teamLinks_elements = browser.find_elements_by_xpath("//a[@class='vereinprofil_tooltip tooltipstered']")
 # print(season_stats_elements)
@@ -62,6 +65,9 @@ for i in teamLinks:
                 continue
             if(url == "https://www.transfermarkt.co.in/phil-ofosu-ayeh/profil/spieler/133858"):
                 continue
+            if(url == "https://www.transfermarkt.co.in/david-brooks/profil/spieler/277033"):
+                continue
+
 
             page = browser.get(url)
             season_stats_elements = browser.find_elements_by_xpath("//a[@class='trackingLDWidget']")
@@ -76,12 +82,12 @@ for i in teamLinks:
                 season_stats_elements = browser.find_elements_by_xpath("//a[@class='trackingLDWidget']")
             except:
                 pass
-
+        name = " ".join(list(map(lambda x:x.capitalize(), url.split("/")[3].split("-"))))
         season_stats = [x.text for x in season_stats_elements[6:12]]
+        cntt+=1
+        row.append([cntt,name,season_stats[0],season_stats[1], season_stats[2], season_stats[4],season_stats[5], ""])
+        print(row)
 
-        print("Games ", season_stats[0])
-        print("Yellow cards ", season_stats[1])
-        print("Goals ", season_stats[2])
-        print("Second yellows ", season_stats[3])
-        print("Assists ", season_stats[4])
-        print("Red cards ", season_stats[5])
+with open('Seaon_player.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(row)
