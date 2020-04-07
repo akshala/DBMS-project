6491d3f4b53e5880bce5f784968e0013e2ffa8d3
Rachit Mittal <rachit18302@iiitd.ac.in>
2020-04-07 19:33:42 +0530

    added serie a and premier league players

--- Season_Player.py
+++ Season_Player.py
@@ -11,41 +11,44 @@ option = webdriver.ChromeOptions()
 option.add_argument(" — incognito")
 
 browser = webdriver.Chrome(executable_path = '/home/rachit/Downloads/chromedriver_linux64/chromedriver', chrome_options=option)
-url = "https://www.transfermarkt.co.in/premier-league/startseite/wettbewerb/GB1"
-page = browser.get(url)
+# url = "htt/ps://www.transfermarkt.com/laliga/startseite/wettbewerb/ES1"
+# page = browser.get(url)
 
-teamLinks_elements = browser.find_elements_by_xpath("//a[@class='vereinprofil_tooltip tooltipstered']")
+# teamLinks_elements = browser.find_elements_by_xpath("//a[@class='vereinprofil_tooltip tooltipstered']")
 # print(season_stats_elements)
-teamLinks = [x.get_attribute("href") for x in teamLinks_elements]
+# teamLinks = [x.get_attribute("href") for x in teamLinks_elements]
 
-teamLinks = list(dict.fromkeys(teamLinks))
-teamLinks = teamLinks[:20]
-print(teamLinks)
+# teamLinks = list(dict.fromkeys(teamLinks))
+# teamLinks = teamLinks[:20]
+# print(teamLinks)
 row = []
-row.append(["S.No","Name", "Games_Played","Goals","Assists","Yellow_cards","Red_cards","Club"])
-cntt = 0
-while (not teamLinks):
-    teamLinks_elements = browser.find_elements_by_xpath("//a[@class='vereinprofil_tooltip tooltipstered']")
-# print(season_stats_elements)
-    teamLinks = [x.get_attribute("href") for x in teamLinks_elements]
+# row.append(["S.No","Name", "Games_Played","Goals","Assists","Yellow_cards","Red_cards","Club"])
+cntt = 1024
+# while (not teamLinks):
+#     teamLinks_elements = browser.find_elements_by_xpath("//a[@class='vereinprofil_tooltip tooltipstered']")
+# # print(season_stats_elements)
+#     teamLinks = [x.get_attribute("href") for x in teamLinks_elements]
 
-    teamLinks = list(dict.fromkeys(teamLinks))
-    teamLinks = teamLinks[:20]
-    # print(teamLinks)
+#     teamLinks = list(dict.fromkeys(teamLinks))
+#     teamLinks = teamLinks[:20]
+#     print(teamLinks)
 
-    playerLinks_elements = []
+#     playerLinks_elements = []
+teamLinks = ['https://www.transfermarkt.com/real-madrid/startseite/verein/418/saison_id/2019', 'https://www.transfermarkt.com/fc-barcelona/startseite/verein/131/saison_id/2019', 'https://www.transfermarkt.com/atletico-madrid/startseite/verein/13/saison_id/2019', 'https://www.transfermarkt.com/fc-valencia/startseite/verein/1049/saison_id/2019', 'https://www.transfermarkt.com/real-sociedad-san-sebastian/startseite/verein/681/saison_id/2019', 'https://www.transfermarkt.com/fc-sevilla/startseite/verein/368/saison_id/2019', 'https://www.transfermarkt.com/real-betis-sevilla/startseite/verein/150/saison_id/2019', 'https://www.transfermarkt.com/athletic-bilbao/startseite/verein/621/saison_id/2019', 'https://www.transfermarkt.com/fc-villarreal/startseite/verein/1050/saison_id/2019', 'https://www.transfermarkt.com/fc-getafe/startseite/verein/3709/saison_id/2019', 'https://www.transfermarkt.com/celta-vigo/startseite/verein/940/saison_id/2019', 'https://www.transfermarkt.com/espanyol-barcelona/startseite/verein/714/saison_id/2019', 'https://www.transfermarkt.com/deportivo-alaves/startseite/verein/1108/saison_id/2019', 'https://www.transfermarkt.com/ud-levante/startseite/verein/3368/saison_id/2019', 'https://www.transfermarkt.com/cd-leganes/startseite/verein/1244/saison_id/2019', 'https://www.transfermarkt.com/real-valladolid/startseite/verein/366/saison_id/2019', 'https://www.transfermarkt.com/rcd-mallorca/startseite/verein/237/saison_id/2019', 'https://www.transfermarkt.com/ca-osasuna/startseite/verein/331/saison_id/2019', 'https://www.transfermarkt.com/sd-eibar/startseite/verein/1533/saison_id/2019', 'https://www.transfermarkt.com/fc-granada/startseite/verein/16795/saison_id/2019']
+
 for i in teamLinks:
-    try:
-        url = i
-        page = browser.get(url)
-        playerLinks_elements = browser.find_elements_by_xpath("//a[@class='spielprofil_tooltip tooltipstered']")
-
+    # try:
+    url = i
+    page = browser.get(url)
+    playerLinks_elements = browser.find_elements_by_xpath("//a[@class='spielprofil_tooltip tooltipstered']")
+    # print("yo")
         # playerLinks= [x.get_attribute("href") for x in playerLinks_elements]
-    except:
-        pass
+    # except:
+        # pass
     while(not playerLinks_elements):
         url = i
         page = browser.get(url)
+        print("yo")
         playerLinks_elements = browser.find_elements_by_xpath("//a[@class='spielprofil_tooltip tooltipstered']")
 
     playerLinks= [x.get_attribute("href") for x in playerLinks_elements]
@@ -82,6 +85,7 @@ for i in teamLinks:
                 url = j
                 page = browser.get(url)
                 season_stats_elements = browser.find_elements_by_xpath("//a[@class='trackingLDWidget']")
+                print(season_stats_elements)
                 maxRetries+=1
             except:
                 pass
@@ -99,6 +103,6 @@ for i in teamLinks:
         row.append([cntt,name,season_stats[0], season_stats[2],season_stats[-2],season_stats[1],season_stats[-1], club])
         print(row)
 
-with open('Seaon_player.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(row)
+# with open('Seaon_player.csv', 'w', newline='') as file:
+#     writer = csv.writer(file)
+#     writer.writerows(row)
