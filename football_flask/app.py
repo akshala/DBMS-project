9da473877b7c5785e2fd8e008e9a36a04828b6d6
Akshala Bhatnagar <47988358+akshala@users.noreply.github.com>
2020-04-25 22:06:30 +0530

    player query done

--- football_flask/app.py
+++ football_flask/app.py
@@ -26,62 +26,66 @@ mycursor = mydb.cursor()
 
 @app.route("/",methods = ['POST', 'GET', 'OPTIONS'])
 def getPage_index():
-	if request.method == "POST":
-		uname = request.form["uname"]
-		password = request.form["pwd"]
-		print("yes")
-		print(uname,password)
-		if (uname == "player" and password == "Player@123"):
-			return render_template('player.html', r=[])
-	print("fkn hell")
-	return render_template('index.html')
+	# if request.method == "POST":
+	# 	uname = request.form["uname"]
+	# 	password = request.form["pwd"]
+	# 	print("yes")
+	# 	print(uname,password)
+	# 	if (uname == "player" and password == "Player@123"):
+	# 		return render_template('player.html', r=[])
+	# print("fkn hell")
+	return render_template('player.html', r=[])
 
 @app.route("/contact_us")
 def getPage_contact_us():
     return render_template('contact.html')
 
-@app.route("/player")
-def getPage_player():
-    return render_template('player.html')
-
-@app.route("/player_data", methods=['POST'])
+@app.route("/player_data", methods=['POST', 'GET'])
 def getData_player():
-	Position = request.form['Position']
-	Club = request.form['Club']
-	Games_Played = request.form['Games_Played']
-	Goals = request.form['Goals']
-	Assists = request.form['Assists']
-	GoalsConceded = request.form['GoalsConceded']
-	CleanSheets = request.form['CleanSheets']
-	Age = request.form['Age']
-	MarketValue = request.form['MarketValue']
-	Height = request.form['Height']
+	if request.method == 'POST':
+		Position = request.form['Position']
+		Club = request.form['Club']
+		Games_Played = request.form['Games_Played']
+		Goals = request.form['Goals']
+		Assists = request.form['Assists']
+		GoalsConceded = request.form['GoalsConceded']
+		CleanSheets = request.form['CleanSheets']
+		Age = request.form['Age']
+		MarketValue = request.form['MarketValue']
+		Height = request.form['Height']
 
-	print(Position, Games_Played, flush=True)
-
-	sql_cmd = "SELECT * from Player where Position=\'{}\' and Club=\'{}\' and Games_Played>={} and Goals>={} and Assists>={} and GoalsConceded>={} and CleanSheets>={} and Age>={} and MarketValue>={} and Height>={}".format(Position, Club, Games_Played, Goals, Assists, GoalsConceded, CleanSheets, Age, MarketValue, Height)
-	mycursor.execute(sql_cmd)
-	data = mycursor.fetchall() # data comes in the form of a list 
-	print("data", data, flush=True)
-	result = []
-	for entries in data:
-		result.append({
-			'Player_ID': int(entries[0]),
-			'Name': str(entries[1]),
-			'Games_Played': int(entries[2]),
-			'Goals': int(entries[3]),
-			'Assists': int(entries[4]),
-			'GoalsConceded': int(entries[5]),
-			'CleanSheets': int(entries[6]),
-			'Position': str(entries[7]),
-			'Age': int(entries[8]),
-			'Contract': str(entries[9]),
-			'MarketValue': float(entries[10]),
-			'Height': float(entries[11]),
-			'Club': str(entries[12]),
-		})
-	print(result, flush=True)
-	return render_template('player.html', r=result)
+		print(Position, Club, flush=True)
+		if(Position == 'None' and Club == 'None'):
+			sql_cmd = "SELECT * from Player where Games_Played>={} and Goals>={} and Assists>={} and GoalsConceded>={} and CleanSheets>={} and Age>={} and MarketValue>={} and Height>={}".format(Games_Played, Goals, Assists, GoalsConceded, CleanSheets, Age, MarketValue, Height)
+		elif(Position == 'None' and Club != 'None'):
+			sql_cmd = "SELECT * from Player where Club=\'{}\' and Games_Played>={} and Goals>={} and Assists>={} and GoalsConceded>={} and CleanSheets>={} and Age>={} and MarketValue>={} and Height>={}".format(Club, Games_Played, Goals, Assists, GoalsConceded, CleanSheets, Age, MarketValue, Height)
+		elif(Position != 'None' and Club == 'None'):
+			sql_cmd = "SELECT * from Player where Position=\'{}\' and Games_Played>={} and Goals>={} and Assists>={} and GoalsConceded>={} and CleanSheets>={} and Age>={} and MarketValue>={} and Height>={}".format(Position, Games_Played, Goals, Assists, GoalsConceded, CleanSheets, Age, MarketValue, Height)
+		else:
+			sql_cmd = "SELECT * from Player where Position=\'{}\' and Club=\'{}\' and Games_Played>={} and Goals>={} and Assists>={} and GoalsConceded>={} and CleanSheets>={} and Age>={} and MarketValue>={} and Height>={}".format(Position, Club, Games_Played, Goals, Assists, GoalsConceded, CleanSheets, Age, MarketValue, Height)
+		print(sql_cmd)
+		mycursor.execute(sql_cmd)
+		data = mycursor.fetchall() # data comes in the form of a list 
+		print("data", data, flush=True)
+		result = []
+		for entries in data:
+			result.append({
+				'Player_ID': int(entries[0]),
+				'Name': str(entries[1]),
+				'Games_Played': int(entries[2]),
+				'Goals': int(entries[3]),
+				'Assists': int(entries[4]),
+				'GoalsConceded': int(entries[5]),
+				'CleanSheets': int(entries[6]),
+				'Position': str(entries[7]),
+				'Age': int(entries[8]),
+				'Contract': str(entries[9]),
+				'MarketValue': float(entries[10]),
+				'Height': float(entries[11]),
+				'Club': str(entries[13]),
+			})
+		print(result, flush=True)
+		return render_template('player.html', r=result)
 
 @app.route('/club/Premier-League', methods=['GET', 'OPTIONS'])
 def getData_club_Premier_League():
